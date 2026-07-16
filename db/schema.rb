1781ec76f80a55c18279b6f1084cacac4de66521
yamakoto1124 <293471220+yamakoto1124@>
2026-07-16 08:33:28 +0000

    [update]ユーザ認証機能作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_07_16_053249) do
+ActiveRecord::Schema[8.0].define(version: 2026_07_16_074647) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -53,6 +53,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_07_16_053249) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
